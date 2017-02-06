7357e92654cffd2f9d744edc9f3f60ad02c86543
Ian Kim <me@ian.kim>
2017-02-06 10:21:15 -0500

    Refactor sequences into new file

--- main.py
+++ main.py
@@ -1,67 +1,6 @@
 from __future__ import absolute_import, print_function
-import re
 from nltk.corpus import cmudict
-
-BILABIAL = ("P", "B", "M", "W")
-ALVEOLAR = ("T", "D", "S", "Z", "N", "L")
-PALATAL = ("SH", "ZH", "CH", "JH", "Y", "R")
-VELAR = ("K", "G", "NG", "HH")
-DENTAL = ("F", "V", "TH", "DH")
-VOWEL = ("AA", "AE", "AH", "AO", "AW", "AY",
-         "EH", "EY",
-         "IH", "IY",
-         "OW", "OY", "UH", "UW", "ER")  # n.b. ER: should be a palatal
-SEQUENCE_REGEX = r"(?P<start>[BAPVD])-(?P<end>[BAPVD])(?P<syllables>\d)"
-SEQUENCE_MATCHER = re.compile(SEQUENCE_REGEX)
-
-
-def segment_to_regex(segment):
-    format_string = r"({})"
-    if segment == VOWEL:
-        format_string += "\d"
-    return format_string.format("|".join(segment))
-
-
-class Sequence(object):
-    def __init__(self, sequence_string):
-        d = SEQUENCE_MATCHER.search(sequence_string).groupdict()
-        self.start = d['start']
-        self.end = d['end']
-        self.syllables = int(d['syllables'])
-
-        segments = []
-        for s in (self.start, self.end):
-            # separate non-empty syllables with 
-            if segments:
-                segments.append(VOWEL)
-
-            if s == "B":
-                segments.append(BILABIAL)
-            elif s == "A":
-                segments.append(ALVEOLAR)
-            elif s == "P":
-                segments.append(PALATAL)
-            elif s == "V":
-                segments.append(VELAR)
-            elif s == "D":
-                segments.append(DENTAL)
-
-        regex_parts = [segment_to_regex(s) for s in segments]
-        self.regex = re.compile(r"\t".join(regex_parts))
-
-    def matches(self, tokens):
-        m = self.regex.search("\t".join(tokens))
-        s = num_syllables(tokens)
-        return m is not None and s == self.syllables
-
-
-def num_syllables(tokens):
-    count = 0
-    for token in tokens:
-        if token[-1].isdigit():
-            count += 1
-    return count
-
+from sequences import Sequence
 
 if __name__ == "__main__":
     s = Sequence(raw_input("Sequence: "))
