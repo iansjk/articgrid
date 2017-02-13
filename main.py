da3a8949b78a7a4a0de9a4b6c654bf704e64ece6
Ian Kim <me@ian.kim>
2017-02-13 11:57:31 -0500

    Add surname filtering support

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import, print_function
 from nltk.corpus import cmudict
 from sequences import Sequence
+from surnames import surnames
 
 if __name__ == "__main__":
     s = Sequence(raw_input("Sequence: "))
     d = cmudict.dict()
     for word in d:
         tokens = d[word][0]
-        if s.matches(tokens):
+        if s.matches(tokens) and word not in surnames:
             print(word)
