ccfc92cb934aaff7143afd9abfa7b92f145862cc
Ian Kim <me@ian.kim>
2017-02-24 09:23:39 -0500

    Check against unix wordlist rather than surnames
    This is a simpler method because there are some unusual surname
    spellings that are not included in the surnames list. Overall this
    should give a more useful list of matching words compared to filtering
    out the surnames.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from __future__ import absolute_import, print_function
-from nltk.corpus import cmudict
+from nltk.corpus import cmudict, words
 from sequences import Sequence
-from surnames import surnames
 
 if __name__ == "__main__":
     s = Sequence(raw_input("Sequence: "))
     d = cmudict.dict()
+    wordlist = words.words()
+    results = []
     for word in d:
         tokens = d[word][0]
-        if s.matches(tokens) and word not in surnames:
-            print(word)
+        if s.matches(tokens) and word in wordlist:
+            results.append(word)
+
+    results.sort()
+    for result in results:
+        print(result)
