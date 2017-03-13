98941404f30995fcec9c02d3b4a8ad4322dfa249
Ian Kim <me@ian.kim>
2017-03-13 19:53:58 -0400

    Add frontend for minimal pairs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, url_for
 
 from images.arasaac import find_images
 from targets.minimal_pairs import find_minimal_pairs
@@ -6,26 +6,53 @@ from targets.minimal_pairs import find_minimal_pairs
 app = Flask(__name__)
 
 
+@app.context_processor
+def inject_navigation():
+    return {
+        "active_page": "Home",
+        "navigation": (
+            ("Home", "/"),
+            ("Images", url_for("images")),
+            ("Minimal Pairs", url_for("minimal_pairs")),
+            ("Moving Across Syllables", url_for("moving_across_syllables")),
+        )}
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/image-search/<query>")
+@app.route("/images/")
+def images():
+    return render_template("images.html")
+
+
+@app.route("/images/search/<query>")
 def image_search(query):
     return json.jsonify({
         "results": find_images(query),
     })
 
 
-@app.route("/minimal-pair-search")
+@app.route("/minimal-pairs/")
+def minimal_pairs():
+    return render_template("minimal-pairs.html")
+
+
+@app.route("/minimal-pairs/search", methods=["POST"])
 def minimal_pair_search():
-    target1 = request.args.get("target1")
-    target2 = request.args.get("target2")
-    position = request.args.get("position")
+    target1 = request.form["target1"]
+    target2 = request.form["target2"]
+    position = request.form["position"]
     return json.jsonify({
         "results": find_minimal_pairs(target1, target2, position),
     })
+
+
+@app.route("/moving-across-syllables/")
+def moving_across_syllables():
+    return render_template("moving-across-syllables.html")
 
 
 if __name__ == "__main__":
---
+++ static/minimal-pairs.js
@@ -0,0 +1,17 @@
+(function () {
+    $(document).ready(function () {
+        $("form").submit(function (e) {
+            e.preventDefault();
+            var form = $(this);
+            var url = form.attr("action");
+            $.post(url, form.serialize(), function (data) {
+                var results = $("#results").show().find("tbody");
+                $.each(data.results, function (i, item) {
+                    var row = $("<tr>").appendTo(results);
+                    row.append($("<td>").text(item[0]));
+                    row.append($("<td>").text(item[1]));
+                });
+            });
+        });
+    });
+})();
