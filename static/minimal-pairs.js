e9a3f82305634f959f774b3a95ac4b2320e82ec0
Ian Kim <me@ian.kim>
2017-03-13 19:55:00 -0400

    Empty result table for subsequent requests

--- static/minimal-pairs.js
+++ static/minimal-pairs.js
@@ -5,7 +5,7 @@
             var form = $(this);
             var url = form.attr("action");
             $.post(url, form.serialize(), function (data) {
-                var results = $("#results").show().find("tbody");
+                var results = $("#results").show().find("tbody").empty();
                 $.each(data.results, function (i, item) {
                     var row = $("<tr>").appendTo(results);
                     row.append($("<td>").text(item[0]));
