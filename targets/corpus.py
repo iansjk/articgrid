75042faa0bb0bc7595fc7f08946943dc6458549a
Ian Kim <me@ian.kim>
2017-04-25 12:53:39 -0400

    Add skeleton and test case for schwar conversion
    In re. #4.

--- targets/corpus.py
+++ targets/corpus.py
@@ -1,6 +1,11 @@
 from nltk.corpus import cmudict
 from nltk.corpus import words
 
+
+def convert_schwar(pronunciation):
+    """Replaces the Arpabet 'ER' (schwar) with the sequence 'AH R' (schwa + r)."""
+    return pronunciation
+
 _cmudict = cmudict.dict()
 common_words = words.words()
-common_cmudict = {key: _cmudict[key] for key in _cmudict.viewkeys() & common_words}
+common_cmudict = {key: [convert_schwar(pron) for pron in _cmudict[key]] for key in _cmudict.viewkeys() & common_words}
---
+++ targets/test_corpus.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from .corpus import convert_schwar
+
+class TestCorpus(TestCase):
+    def test_convert_schwar(self):
+        tests = {
+            "evaporator": ("IH0 V AE1 P ER0 EY2 T ER0", "IH0 V AE1 P AH0 R EY2 T AH0 R"),
+            "exterminator": ("IH0 K S T ER1 M AH0 N EY2 T ER0", "IH0 K S T AH1 M AH0 N EY2 T AH0"),
+            "fern": ("F ER1 N", "F AH1 R N"),
+            "fetter": ("F EH1 T ER0", "F EH1 T AH0 R")
+        }
+        for word in tests.keys():
+            original, expected = tests[word]
+            self.assertEquals(convert_schwar(original), expected)
