18e625cea833823386fc114d033ae506de907e5b
Ian Kim <me@ian.kim>
2017-04-25 13:54:32 -0400

    Convert schwar in Arpanet to schwa-r sequence
    This fixes #4.

--- targets/corpus.py
+++ targets/corpus.py
@@ -4,7 +4,15 @@ from nltk.corpus import words
 
 def convert_schwar(pronunciation):
     """Replaces the Arpabet 'ER' (schwar) with the sequence 'AH R' (schwa + r)."""
-    return pronunciation
+    new_pronunciation = []
+    for segment in pronunciation:
+        if segment.startswith("ER"):
+            new_pronunciation.append("AH" + segment[2])  # use same stress as the ER
+            new_pronunciation.append("R")
+        else:
+            new_pronunciation.append(segment)
+    return new_pronunciation
+
 
 _cmudict = cmudict.dict()
 common_words = words.words()
