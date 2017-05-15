0821a3a984f92f0b4d3cc4727dc2e59336af6cb8
Ian Kim <iansjk@gmail.com>
2017-05-15 02:02:52 -0400

    Separate css/js from html

---
+++ static/grid-builder.js
@@ -0,0 +1,39 @@
+"use strict";
+
+$(document).ready(function () {
+    var $prototype = $("#prototype").children();
+    var $gridsize = $("#grid-size");
+    var $gridcells = $("#grid-cells");
+    $gridsize.change(function () {
+        $gridcells.empty();
+        for (var i = 0; i < $gridsize.val(); i++) {
+            var $row = $('<div class="row">');
+            for (var j = 0; j < $gridsize.val(); j++) {
+                $prototype.clone().appendTo($row);
+            }
+            $row.appendTo($gridcells);
+        }
+    }).trigger("change");
+
+    $("#print").click(function (e) {
+        e.preventDefault();
+        window.print();
+    });
+
+    // disallow newline (enter key) in grid title
+    $("#grid-title").keypress(function (e) {
+        return e.which !== 13;
+    });
+
+    $("h2, h4").focusin(function () {
+        var $this = $(this);
+        if ($this.text().trim() === $this.attr("data-placeholder")) {
+            $this.text("");
+        }
+    }).focusout(function () {
+        var $this = $(this);
+        if ($this.text().trim() === "") {
+            $this.text($this.attr("data-placeholder"));
+        }
+    });
+});
