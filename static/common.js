4729b5efff066e66ab13f48fb3b333fbddf5c047
Ian Kim <iansjk@gmail.com>
2017-05-15 02:10:08 -0400

    Rename selector vars for consistency

--- static/common.js
+++ static/common.js
@@ -10,11 +10,11 @@ $(document).ready(function () {
     $("#results").not('[data-defer-init="true"]').DataTable(DATATABLE_OPTIONS);
     $("form[action]").submit(function (e) {
         e.preventDefault();
-        var form = $(this);
-        var url = form.attr("action");
-        var submit = form.find('button[type="submit"]').prop("disabled", true).text("Working...");
-        $("#results").DataTable().ajax.url(url + "?" + form.serialize()).load(function () {
-            submit.prop("disabled", false).text("Search");
+        var $form = $(this);
+        var url = $form.attr("action");
+        var $submit = $form.find('button[type="submit"]').prop("disabled", true).text("Working...");
+        $("#results").DataTable().ajax.url(url + "?" + $form.serialize()).load(function () {
+            $submit.prop("disabled", false).text("Search");
         });
     })
 });
--- static/pictograms.js
+++ static/pictograms.js
@@ -1,7 +1,7 @@
 "use strict";
 
 $(document).ready(function () {
-    var results = $("#results").DataTable($.extend(DATATABLE_OPTIONS, {
+    $("#results").DataTable($.extend(DATATABLE_OPTIONS, {
         "columns": [
             null,
             {
