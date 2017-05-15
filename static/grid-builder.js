0bff99e5c9ac99207b4bbba65e6b9c9ec5f15045
Ian Kim <iansjk@gmail.com>
2017-05-15 17:06:03 -0400

    Implement image selection and replacement

--- static/grid-builder.js
+++ static/grid-builder.js
@@ -38,16 +38,70 @@
             }
         });
 
+        var $imageResults = $("#image-results");
+        var $imageSearchQuery = $("#image-search-query");
+        var typingTimer;
+        var doneTypingInterval = 2000;
+
+        $imageSearchQuery.on("input propertychange paste", function () {
+            clearTimeout(typingTimer);
+            typingTimer = setTimeout(doneTyping, doneTypingInterval);
+            $imageResults.empty().siblings(".placeholder").show();
+        });
+
+        function doneTyping() {
+            var $form = $imageSearchQuery.parents("form");
+            var url = $form.attr("action");
+            $.get(url + "?" + $form.serialize(), function (json) {
+                $imageResults.siblings(".placeholder").hide();
+                if (json.data.length === 0) {
+                    $imageResults.html('<span class="no-results">No image results found.</span>');
+                } else {
+                    for (var i = 0; i < json.data.length; i++) {
+                        var celldata = json.data[i];
+                        for (var j = 0; j < celldata[1].length; j++) {
+                            var $col = $('<div class="col-3">').appendTo($imageResults);
+                            $('<img class="img-fluid">').attr("alt", celldata[0]).attr("src", celldata[1][j]).appendTo($col).tooltip({
+                                "animation": false,
+                                "title": function() {
+                                    return this.alt;
+                                }
+                            });
+                        }
+                    }
+                }
+                $imageResults.find("img").imgCheckbox({
+                    "radio": true,
+                    "graySelected": false,
+                    "onclick": function(e) {
+                        console.log(e);
+                    }
+                });
+            });
+        }
+
         var $imagePicker = $("#image-picker");
         $imagePicker.on("show.bs.modal", function (e) {
-            var $cellTitle = $(e.relatedTarget).siblings(".cell-title");
+            var $target = $(e.relatedTarget).attr("id", "target");
+            var $cellTitle = $target.siblings(".cell-title");
             var cellTitleText = $cellTitle.text().trim();
             var $imageSearchQuery = $imagePicker.find("#image-search-query");
             if (cellTitleText !== "" && cellTitleText !== $cellTitle.attr("data-placeholder")) {
-                $imageSearchQuery.val(cellTitleText);
+                $imageSearchQuery.val(cellTitleText).trigger("input");
             } else {
                 $imageSearchQuery.val("");
             }
+        });
+
+        $("#save").click(function(e) {
+            e.preventDefault();
+            var $target = $("#target");
+            var selectedImgUrl = $imageResults.find(".imgChked").children("img").attr("src");
+            if (selectedImgUrl) {
+                $target.attr("src", selectedImgUrl);
+            }
+            $target.removeAttr("id");
+            $imagePicker.modal("hide");
         });
     });
 }());
