69316dabd2f6fc06e74f8b332ff5be2d147cd42d
Ian Kim <iansjk@gmail.com>
2017-06-05 01:44:11 -0400

    Move selectors into document.ready

--- static/grid-builder.js
+++ static/grid-builder.js
@@ -3,106 +3,106 @@
 
     var MINIMUM_SEARCH_LENGTH = 3;
     var TYPING_TIMEOUT = 100; // ms
-
-    var $cellContainer = $("#cell-container");
-    var $gridsize = $("#grid-size");
-    var $gridtitle = $("#grid-title");
-    var $imageResults = $("#image-results");
-    var $imageSearchQuery = $("#image-search-query");
-    var $prototype = $("#prototype").children();
     var imageSearchXHR;
-    var $targetImage;
-
-    function init(savedJson) {
-        $gridtitle.trigger("titleChanged");
-        if (savedJson === undefined) {
-            $gridsize.trigger("change");
-        } else {
-            $gridsize.find('option[value="' + savedJson.size + '"]').prop("selected", true);
-            if (savedJson.title) {
-                $gridtitle.text(savedJson.title).trigger("titleChanged");
-            }
-            var $row = $('<div class="row">').appendTo($cellContainer);
-            for (var i = 0; i < savedJson.cells.length; i++) {
-                if (i % savedJson.size === 0) {
-                    $row = $row.clone().empty().appendTo($cellContainer);
-                }
-                var celldata = savedJson.cells[i];
-                var $cell = $prototype.clone();
-                if (celldata.id) {
-                    $cell.find(".cell-image")
-                        .attr("src", Flask.url_for("static_pictogram", {"pictogram_id": celldata.id}))
-                        .attr("data-pictogram-id", celldata.id);
-                }
-                if (celldata.label) {
-                    $cell.find(".cell-title").text(celldata.label);
-                }
-                $row.append($cell);
-            }
-            $cellContainer.find(".cell").trigger("cellChanged");
-            updateUrl();
-        }
-    }
-
-    function updateUrl() {
-        var title = $gridtitle.text().trim();
-        var currentState = {
-            "title": title === $gridtitle.attr("data-placeholder") ? "" : title,
-            "size": parseInt($gridsize.val()),
-            "cells": []
-        };
-        var $cells = $cellContainer.find(".cell");
-        for (var i = 0; i < $cells.length; i++) {
-            var $cell = $($cells[i]);
-            if ($cell.hasClass("empty")) {
-                currentState.cells.push({});
-            } else {
-                var $cellTitle = $cell.find(".cell-title");
-                var label = $cellTitle.text().trim();
-                currentState.cells.push({
-                    "id": parseInt($cell.find(".cell-image").attr("data-pictogram-id")) || 0,
-                    "label": label === $cellTitle.attr("data-placeholder") ? "" : label
-                });
-            }
-        }
-        window.location.hash = JSON.stringify(currentState);
-    }
-
-    function search() {
-        var $form = $imageSearchQuery.parents("form");
-        var url = $form.attr("action");
-        imageSearchXHR = $.get(url + "?" + $form.serialize(), function (json) {
-            $imageResults.siblings(".placeholder").hide();
-            if (json.data.length === 0) {
-                $imageResults.html('<span class="no-results">No image results found.</span>');
-            } else {
-                for (var i = 0; i < json.data.length; i++) {
-                    var celldata = json.data[i];
-                    for (var j = 0; j < celldata[1].length; j++) {
-                        var $col = $('<div class="col-3">').appendTo($imageResults);
-                        $('<img class="img-fluid">')
-                            .attr("alt", celldata[0])
-                            .attr("data-pictogram-id", celldata[1][j])
-                            .attr("src", Flask.url_for("static_pictogram", {"pictogram_id": celldata[1][j]}))
-                            .attr("width", 80)
-                            .attr("height", 80).appendTo($col).tooltip(
-                            {
-                                "animation": false,
-                                "title": function () {
-                                    return this.alt;
-                                }
-                            });
-                    }
-                }
-            }
-            $imageResults.find("img").imgCheckbox({
-                "radio": true,
-                "graySelected": false
-            });
-        });
-    }
 
     $(document).ready(function () {
+        var $cellContainer = $("#cell-container");
+        var $gridsize = $("#grid-size");
+        var $gridtitle = $("#grid-title");
+        var $imageResults = $("#image-results");
+        var $imageSearchQuery = $("#image-search-query");
+        var $prototype = $("#prototype").children();
+        var $targetImage;
+
+        function init(savedJson) {
+            $gridtitle.trigger("titleChanged");
+            if (savedJson === undefined) {
+                $gridsize.trigger("change");
+            } else {
+                $gridsize.find('option[value="' + savedJson.size + '"]').prop("selected", true);
+                if (savedJson.title) {
+                    $gridtitle.text(savedJson.title).trigger("titleChanged");
+                }
+                var $row = $('<div class="row">').appendTo($cellContainer);
+                for (var i = 0; i < savedJson.cells.length; i++) {
+                    if (i % savedJson.size === 0) {
+                        $row = $row.clone().empty().appendTo($cellContainer);
+                    }
+                    var celldata = savedJson.cells[i];
+                    var $cell = $prototype.clone();
+                    if (celldata.id) {
+                        $cell.find(".cell-image")
+                            .attr("src", Flask.url_for("static_pictogram", {"pictogram_id": celldata.id}))
+                            .attr("data-pictogram-id", celldata.id);
+                    }
+                    if (celldata.label) {
+                        $cell.find(".cell-title").text(celldata.label);
+                    }
+                    $row.append($cell);
+                }
+                $cellContainer.find(".cell").trigger("cellChanged");
+                updateUrl();
+            }
+        }
+
+        function updateUrl() {
+            var title = $gridtitle.text().trim();
+            var currentState = {
+                "title": title === $gridtitle.attr("data-placeholder") ? "" : title,
+                "size": parseInt($gridsize.val()),
+                "cells": []
+            };
+            var $cells = $cellContainer.find(".cell");
+            for (var i = 0; i < $cells.length; i++) {
+                var $cell = $($cells[i]);
+                if ($cell.hasClass("empty")) {
+                    currentState.cells.push({});
+                } else {
+                    var $cellTitle = $cell.find(".cell-title");
+                    var label = $cellTitle.text().trim();
+                    currentState.cells.push({
+                        "id": parseInt($cell.find(".cell-image").attr("data-pictogram-id")) || 0,
+                        "label": label === $cellTitle.attr("data-placeholder") ? "" : label
+                    });
+                }
+            }
+            window.location.hash = JSON.stringify(currentState);
+        }
+
+        function search() {
+            var $form = $imageSearchQuery.parents("form");
+            var url = $form.attr("action");
+            imageSearchXHR = $.get(url + "?" + $form.serialize(), function (json) {
+                $imageResults.siblings(".placeholder").hide();
+                if (json.data.length === 0) {
+                    $imageResults.html('<span class="no-results">No image results found.</span>');
+                } else {
+                    for (var i = 0; i < json.data.length; i++) {
+                        var celldata = json.data[i];
+                        for (var j = 0; j < celldata[1].length; j++) {
+                            var $col = $('<div class="col-3">').appendTo($imageResults);
+                            $('<img class="img-fluid">')
+                                .attr("alt", celldata[0])
+                                .attr("data-pictogram-id", celldata[1][j])
+                                .attr("src", Flask.url_for("static_pictogram", {"pictogram_id": celldata[1][j]}))
+                                .attr("width", 80)
+                                .attr("height", 80).appendTo($col).tooltip(
+                                {
+                                    "animation": false,
+                                    "title": function () {
+                                        return this.alt;
+                                    }
+                                });
+                        }
+                    }
+                }
+                $imageResults.find("img").imgCheckbox({
+                    "radio": true,
+                    "graySelected": false
+                });
+            });
+        }
+
         $("#grid").on("cellChanged", ".cell", function (_, params) {
             var $cell = $(this);
             var $cellTitle = $cell.find(".cell-title");
