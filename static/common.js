59e33dc8bab3dfdbdb2c52195640d4b8b7c3ce8b
Ian Kim <iansjk@gmail.com>
2017-06-05 01:43:16 -0400

    Return pictogram results page-by-page

--- static/common.js
+++ static/common.js
@@ -8,19 +8,12 @@
     };
 
     $(document).ready(function () {
-        $("#results").not('[data-defer-init="true"]').DataTable(window.DATATABLE_OPTIONS);
-        $("form[action]").submit(function (e) {
-            e.preventDefault();
-            var $form = $(this);
-            var url = $form.attr("action");
-            var $submit = $form.find('button[type="submit"]');
-            var origText = $submit.text();
+        var $submit = $("form").find('button[type="submit"]');
+        var origText = $submit.text();
+        $("#results").on("preXhr.dt", function () {
             $submit.width($submit.width()).html('<i class="fa fa-circle-o-notch fa-spin"></i><span class="sr-only">Loading...</span>').prop("disabled", true);
-            $("#results").DataTable().on("xhr.dt", function (_, __, ___, xhr) {
-                xhr.always(function () {
-                    $submit.prop("disabled", false).text(origText);
-                });
-            }).ajax.url(url + "?" + $form.serialize()).load();
+        }).on("xhr.dt", function () {
+            $submit.prop("disabled", false).text(origText);
         });
     });
 }());
