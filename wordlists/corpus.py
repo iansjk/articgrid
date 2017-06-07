ad0fd6d081f922d899f1dfccf45a0edbff053538
Ian Kim <me@ian.kim>
2017-06-06 23:30:48 -0400

    Add Arpanet to IPA correspondence dict

--- wordlists/corpus.py
+++ wordlists/corpus.py
@@ -4,6 +4,48 @@ from nltk.corpus import words
 
 consonants = {"B", "CH", "D", "DH", "F", "G", "HH", "JH", "K", "L", "M", "N", "NG", "P", "R", "S", "SH", "T", "TH", "V",
               "W", "Y", "Z", "ZH"}
+arpabet_to_ipa = {
+    "AA": u"ɑ",
+    "AE": u"æ",
+    "AH1": u"ʌ",
+    "AH0": u"ə",
+    "AO": u"ɔ",
+    "B": u"b",
+    "CH": u"t͡ʃ",
+    "D": u"d",
+    "DH": u"ð",
+    "EH": u"ɛ",
+    "ER1": u"ɝ",
+    "ER0": u"ɚ",
+    "EY": u"aɪ",
+    "F": u"f",
+    "G": u"g",
+    "HH": u"h",
+    "IH": u"ɪ",
+    "IY": u"i",
+    "JH": u"d͡ʒ",
+    "K": u"k",
+    "L": u"l",
+    "M": u"m",
+    "N": u"n",
+    "NG": u"ŋ",
+    "OW": u"oʊ",
+    "OY": u"ɔɪ",
+    "P": u"p",
+    "R": u"ɹ",
+    "S": u"s",
+    "SH": u"ʃ",
+    "T": u"t",
+    "TH": u"θ",
+    "UH": u"ʊ",
+    "UW": u"u",
+    "V": u"v",
+    "W": u"w",
+    "Y": u"j",
+    "Z": u"z",
+    "ZH": u"ʒ"
+}
+
 cmudict = raw_cmudict.dict()
 unix_words = set(words.words())
 
