d136ce4a3542dce0e62346ed049744f13f304d0e
Ian Kim <me@ian.kim>
2017-07-20 10:35:43 -0400

    Speed up frequency processing

--- artictools/wordlists/frequency.py
+++ artictools/wordlists/frequency.py
@@ -4,11 +4,9 @@ from io import BytesIO
 from zipfile import ZipFile
 
 import requests
-from backports import csv
 
 import artictools
 
-ANC_FIELDNAMES = ("word", "lemma", "pos", "count")
 ANC_RAW_DATA_URL = "http://www.anc.org/SecondRelease/data/ANC-all-count.zip"
 ANC_DATA_FILENAME = "ANC-all-count.txt"
 BASE_DIR = os.path.dirname(artictools.__file__)
@@ -28,18 +26,19 @@ def _is_word_excluded(word):
     return not word or word[0] == "'"
 
 
-def _is_row_excluded(row):
-    return _is_word_excluded(row["word"]) or row["count"] is None
+def _is_row_excluded(word, raw_count):
+    return _is_word_excluded(word) or raw_count is None
 
 
 frequency = {}
 with codecs.open(get_anc_data(), "r", encoding="latin-1") as csvfile:
-    reader = csv.DictReader(csvfile, fieldnames=ANC_FIELDNAMES, dialect="excel-tab")
-    for row in reader:
-        if _is_row_excluded(row):
+    for row in csvfile.read().splitlines():
+        try:
+            (word, _, __, raw_count) = row.split("\t")
+        except ValueError:
             continue
 
-        word, count = row["word"], int(row["count"])
+        count = int(raw_count)
         if word not in frequency:
             frequency[word] = count
         else:
