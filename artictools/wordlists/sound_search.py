ed1ee8d402e70cb2cf782c84480b1baa14635c7b
Ian Kim <me@ian.kim>
2017-07-20 13:48:50 -0400

    Add wildcard consonants to sound search
    This fixes #13.

--- artictools/wordlists/sound_search.py
+++ artictools/wordlists/sound_search.py
@@ -1,11 +1,23 @@
 import six
 
-from artictools.wordlists.corpus import words
+from artictools.wordlists.corpus import words, CONSONANTS
+
+
+def sequence_matches(sequence1, sequence2):
+    return len(sequence1) == len(sequence2) and \
+           all([sound1 == sound2 or (sound1 == "*" and sound2 in CONSONANTS) or \
+                (sound1 in CONSONANTS and sound2 == "*") for sound1, sound2 in zip(sequence1, sequence2)])
+
+
+def pronunciation_matches(pronunciation, position, targets):
+    return position == "initial" and sequence_matches(pronunciation[:len(targets)], targets) or \
+           position == "final" and sequence_matches(pronunciation[-len(targets):], targets) or \
+           position == "medial" and sublist_in_list(targets, pronunciation[1:-1])
 
 
 def sublist_in_list(sublist, full_list):
     for i in (index for index, token in enumerate(full_list) if token == sublist[0]):
-        if full_list[i:i + len(sublist)] == list(sublist):
+        if sequence_matches(full_list[i:i + len(sublist)], list(sublist)):
             return True
     return False
 
@@ -16,10 +28,8 @@ def find_sound_sequence(position, *targets):
         for pronunciation in entry.pronunciations:
             # ensure word is at least as long as the target sequence; this prevents weirdness like "str" initial
             # returning the word "st" (an abbreviation for "street" which does start with the "str" sequence)
-            if len(pronunciation) >= len(targets) and len(word) >= len(targets):
-                if position == "initial" and pronunciation[:len(targets)] == list(targets) or \
-                                        position == "final" and pronunciation[-len(targets):] == list(targets) or \
-                                        position == "medial" and sublist_in_list(targets, pronunciation[1:-1]):
-                    results.append(word)
-                    break
+            if len(pronunciation) >= len(targets) and len(word) >= len(targets) and \
+                    pronunciation_matches(pronunciation, position, targets):
+                results.append(word)
+                break
     return results
--- artictools/wordlists/test_sound_search.py
+++ artictools/wordlists/test_sound_search.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 import six
 
-from artictools.wordlists.sound_search import find_sound_sequence
+from artictools.wordlists.sound_search import find_sound_sequence, sequence_matches
 
 
 class TestSoundSearch(TestCase):
@@ -18,6 +18,9 @@ class TestSoundSearch(TestCase):
         ("S", "K", "Y"): "skew"
     }
     NOT_CODA = {"HH", "W", "Y"}
+    R_BLENDS = {("P", "R"), ("B", "R"), ("T", "R"), ("D", "R"), ("K", "R"), ("G", "R"), ("F", "R")}
+    S_BLENDS = {("S", "K"), ("S", "N"), ("S", "T"), ("S", "L"), ("S", "P"), ("S", "W"), ("S", "M")}
+    L_BLENDS = {("P", "L"), ("B", "L"), ("K", "L"), ("G", "L"), ("F", "L")}
 
     def test_single(self):
         # NG can only be a coda
@@ -35,6 +38,10 @@ class TestSoundSearch(TestCase):
         # conversely make sure consonant + schwar is not
         self.assertNotIn("walker", find_sound_sequence("final", "K", "R"))
 
+        # wildcards should match consonants only
+        self.assertNotIn("tilt", find_sound_sequence("initial", "T", "*"))
+        self.assertIn("trip", find_sound_sequence("initial", "T", "*"))
+
     def test_triple_onsets(self):
         for triple, example in six.iteritems(self.TRIPLE_ONSETS):
             result = find_sound_sequence("initial", *triple)
@@ -47,3 +54,23 @@ class TestSoundSearch(TestCase):
         self.assertGreater(len(edh_medial_words), 0)
         self.assertIn("brother", edh_medial_words)
         self.assertNotIn("there", edh_medial_words)
+
+        medial_st_blends = find_sound_sequence("medial", "S", "T")
+        self.assertTrue(medial_st_blends)
+
+    def test_sequence_matches(self):
+        for s_blend in self.S_BLENDS:
+            self.assertTrue(sequence_matches(["S", "*"], s_blend))
+            self.assertTrue(sequence_matches(s_blend, ["S", "*"]))
+            self.assertFalse(sequence_matches(s_blend, ["*", "S"]))
+        for l_blend in self.L_BLENDS:
+            self.assertTrue(sequence_matches(["*", "L"], l_blend))
+            self.assertTrue(sequence_matches(l_blend, ["*", "L"]))
+            self.assertFalse(sequence_matches(l_blend, ["L", "*"]))
+        for r_blend in self.R_BLENDS:
+            self.assertTrue(sequence_matches(["*", "R"], r_blend))
+            self.assertTrue(sequence_matches(r_blend, ["*", "R"]))
+            self.assertFalse(sequence_matches(r_blend, ["R", "*"]))
+        for triple in self.TRIPLE_ONSETS:
+            self.assertTrue(sequence_matches(["S", "*", "*"], triple))
+            self.assertTrue(sequence_matches(triple, ["S", "*", "*"]))
