e8be2f8266fdd46429d6ab8d41dc8854601a3473
Ian Kim <me@ian.kim>
2017-11-08 17:20:02 -0500

    Fix jshint path

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
             options: {
                 jshintrc: '.jshintrc'
             },
-            all: ['static/*.js']
+            all: ['artictools/static/*.js']
         },
         bowercopy: {
             scripts: {
